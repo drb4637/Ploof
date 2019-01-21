551acc8d12d6b0d02795add11eea31c60e1963f0
Daniel Bower <drb4637@gmail.com>
2019-01-21 14:20:55 -0500

    added another test

--- test.py
+++ test.py
@@ -10,6 +10,12 @@ def test_answer2():
 def test_answer3(): 
 	assert 1 == 1  
 
+def test_answer31(): 
+	assert 1 == 1  
+
+def test_answer32(): 
+	assert 1 == 1  
+
 def test_answer4(): 
 	assert 2 == 1  
 
